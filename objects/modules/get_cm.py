ccdf1f080c789c2e8810586e2f3e354374d967a8
dguyhasnoname <dguyhasnoname@gmail.com>
2020-07-30 14:54:57 +0530

    adding cm module

---
+++ objects/modules/get_cm.py
@@ -0,0 +1,19 @@
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+
+config.load_kube_config()
+core = client.CoreV1Api()
+
+class K8sConfigMap:
+    def get_cm(ns):
+        try:
+            if ns != 'all':
+                #print ("\n[INFO] Fetching {} namespace configMaps data...".format(ns))
+                namespace = ns
+                configmaps = core.list_namespaced_config_map(namespace, timeout_seconds=10)
+            else:
+                print ("\n[INFO] Fetching all namespace configMaps data...")
+                configmaps = core.list_config_map_for_all_namespaces(timeout_seconds=10)
+            return configmaps
+        except ApiException as e:
+            print("Exception when calling CoreV1Api->list_namespaced_config_map: %s\n" % e)
