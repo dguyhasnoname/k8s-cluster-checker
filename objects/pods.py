ae4e79eb4b425b3f52d4f5d3f3bb7b04af29672d
dguyhasnoname <dguyhasnoname@gmail.com>
2020-08-08 08:45:55 +0530

    removed Output

--- objects/pods.py
+++ objects/pods.py
@@ -48,33 +48,27 @@ class _Pods:
     def check_pod_security(v):
         headers = ['NAMESPACE', 'POD', 'CONTAINER_NAME', 'PRIVILEGED_ESC', \
         'PRIVILEGED', 'READ_ONLY_FS', 'RUN_AS_NON_ROOT', 'RUNA_AS_USER']        
-        data = k8s.Check.security_context(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)    
+        k8s.Check.security_context(k8s_object,k8s_object_list,headers,v)
 
     def check_pod_health_probes(v):
         headers = ['NAMESPACE', 'POD', 'CONTAINER_NAME', 'READINESS_PROPBE', 'LIVENESS_PROBE']        
-        data = k8s.Check.health_probes(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)  
+        k8s.Check.health_probes(k8s_object,k8s_object_list,headers,v)
 
     def check_pod_resources(v):
         headers = ['NAMESPACE', 'POD', 'CONTAINER_NAME', 'LIMITS', 'REQUESTS']       
-        data = k8s.Check.resources(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        k8s.Check.resources(k8s_object,k8s_object_list,headers,v)
 
     def check_pod_qos(v):
         headers = ['NAMESPACE', 'POD', 'QoS']
-        data = k8s.Check.qos(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        k8s.Check.qos(k8s_object,k8s_object_list,headers,v)
 
     def check_pod_tolerations_affinity_node_selector_priority(v):
         headers = ['NAMESPACE', 'POD', 'NODE_SELECTOR', 'TOLERATIONS', 'AFFINITY', 'PRIORITY_CLASS']
-        data = k8s.Check.tolerations_affinity_node_selector_priority(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        k8s.Check.tolerations_affinity_node_selector_priority(k8s_object,k8s_object_list,headers,v)
 
     def check_image_pull_policy(v):
         headers = ['DEPLOYMENT', 'CONTAINER_NAME', 'IMAGE', 'IMAGE_PULL_POLICY', 'LATEST_TAG_AVAILABLE']
-        data = k8s.Check.image_pull_policy(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        k8s.Check.image_pull_policy(k8s_object,k8s_object_list,headers,v)
 
 def call_all(v,ns):
     _Pods(ns)
