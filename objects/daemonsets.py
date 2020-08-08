5a80f5d451d852b87bee03d4ad51c19cbb30bc62
dguyhasnoname <dguyhasnoname@gmail.com>
2020-08-08 08:44:53 +0530

    moved print to modules

--- objects/daemonsets.py
+++ objects/daemonsets.py
@@ -31,23 +31,19 @@ class _Daemonset:
     def check_damemonset_security(v):
         headers = ['NAMESPACE', 'DAEMONSET', 'CONTAINER_NAME', 'PRIVILEGED_ESC', \
         'PRIVILEGED', 'READ_ONLY_FS', 'RUN_AS_NON_ROOT', 'RUNA_AS_USER']        
-        data = k8s.Check.security_context(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        data = k8s.Check.security_context(k8s_object, k8s_object_list, headers, v)
 
     def check_damemonset_health_probes(v):
         headers = ['NAMESPACE', 'DAEMONSET', 'CONTAINER_NAME', 'READINESS_PROPBE', 'LIVENESS_PROBE']        
-        data = k8s.Check.health_probes(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        data = k8s.Check.health_probes(k8s_object, k8s_object_list, headers, v)
 
     def check_damemonset_resources(v):
         headers = ['NAMESPACE', 'DAEMONSET', 'CONTAINER_NAME', 'LIMITS', 'REQUESTS']
-        data = k8s.Check.resources(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        data = k8s.Check.resources(k8s_object, k8s_object_list, headers, v)
 
     def check_damemonset_tolerations_affinity_node_selector_priority(v):  
         headers = ['DAEMONSET', 'NODE_SELECTOR', 'TOLERATIONS', 'AFFINITY', 'PRIORITY_CLASS']
-        data = k8s.Check.tolerations_affinity_node_selector_priority(k8s_object,k8s_object_list)
-        k8s.Output.print_table(data,headers,v)
+        data = k8s.Check.tolerations_affinity_node_selector_priority(k8s_object, k8s_object_list, headers, v)
 
 def call_all(v,ns):
     _Daemonset(ns)
