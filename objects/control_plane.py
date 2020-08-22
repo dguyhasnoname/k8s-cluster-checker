d9e461c20be00cb239792f3a0285fa6092ad096a
dguyhasnoname <dguyhasnoname@gmail.com>
2020-08-22 23:05:21 +0530

    added logging

--- objects/control_plane.py
+++ objects/control_plane.py
@@ -1,14 +1,16 @@
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 import sys, time, os, re, getopt
+start_time = time.time()
+from modules import logging as logger
 from modules import process as k8s
 
-start_time = time.time()
 config.load_kube_config()
 core = client.CoreV1Api()
 
-class CtrlPlane:
-    global k8s_object, k8s_object_list, namespace
+class CtrlPlane:  
+    global k8s_object, k8s_object_list, namespace, _logger
+    _logger = logger.get_logger('CtrlPlane')
     namespace = 'kube-system'
     def check_ctrl_plane_pods():
         try:
@@ -26,7 +28,7 @@ class CtrlPlane:
     k8s_object_list = check_ctrl_plane_pods()
     k8s_object = 'pods'  
 
-    def get_ctrl_plane_pods():
+    def get_ctrl_plane_pods(l):
         if not k8s_object_list: return
         data = []
         headers = ['NAMESPACE', 'PODS', 'NODE_NAME', 'QoS']
@@ -35,35 +37,35 @@ class CtrlPlane:
             item.spec.node_name])
         data = k8s.Output.append_hyphen(data, '------------')
         data.append(["Total pods: ", len(data) - 1, ''])
-        k8s.Output.print_table(data,headers,True)
+        k8s.Output.print_table(data, headers, True, l)
 
-    def check_ctrl_plane_security(v):
+    def check_ctrl_plane_security(v, l):
         headers = ['NAMESPACE', 'POD', 'CONTAINER_NAME', 'PRIVILEGED_ESC', \
         'PRIVILEGED', 'READ_ONLY_FS', 'RUN_AS_NON_ROOT', 'RUNA_AS_USER']        
         k8s.Check.security_context(k8s_object, k8s_object_list, headers, \
-        v, namespace)
+        v, namespace, l)
 
-    def check_ctrl_plane_pods_health_probes(v):
+    def check_ctrl_plane_pods_health_probes(v, l):
         headers = ['NAMESPACE', 'PODS', 'CONTAINER_NAME', 'READINESS_PROPBE', \
         'LIVENESS_PROBE']        
         k8s.Check.health_probes(k8s_object, k8s_object_list, headers, \
-        v, namespace)
+        v, namespace, l)
 
-    def check_ctrl_plane_pods_resources(v):
+    def check_ctrl_plane_pods_resources(v, l):
         headers = ['NAMESPACE', 'PODS', 'CONTAINER_NAME', 'LIMITS', 'REQUESTS']       
-        k8s.Check.resources(k8s_object, k8s_object_list, headers, v, namespace)
+        k8s.Check.resources(k8s_object, k8s_object_list, headers, v, namespace, l)
 
     # gets file name from check_ctrl_plane_pods_properties function
-    def check_ctrl_plane_pods_properties_operation(item, filename, headers, v):
+    def check_ctrl_plane_pods_properties_operation(item, filename, headers, v, l):
         commands = item.spec.containers[0].command
         data = k8s.CtrlProp.compare_properties(filename, commands)
-        k8s.Output.print_table(data, headers, v)
+        k8s.Output.print_table(data, headers, v, l)
 
-    def check_ctrl_plane_pods_qos(v):
+    def check_ctrl_plane_pods_qos(v, l):
         headers = ['NAMESPACE', 'POD', 'QoS']
-        k8s.Check.qos(k8s_object, k8s_object_list, headers, v, namespace)
+        k8s.Check.qos(k8s_object, k8s_object_list, headers, v, namespace, l)
 
-    def check_ctrl_plane_pods_properties(v):
+    def check_ctrl_plane_pods_properties(v, l):
         if not k8s_object_list: return
         container_name_check = ""
         headers = ['CTRL_PLANE_COMPONENT/ARGS', '']
@@ -71,36 +73,39 @@ class CtrlPlane:
             if item.spec.containers[0].name in "kube-controller-manager" \
             and item.spec.containers[0].name not in container_name_check:
                 CtrlPlane.check_ctrl_plane_pods_properties_operation(item,\
-                './conf/kube-controller-manager', headers, v)
+                './conf/kube-controller-manager', headers, v, l)
 
             elif item.spec.containers[0].name in "kube-apiserver" \
             and item.spec.containers[0].name not in container_name_check:
                 CtrlPlane.check_ctrl_plane_pods_properties_operation(item,\
-                './conf/kube-apiserver', headers, v)                
-                k8s.CtrlProp.check_admission_controllers(\
-                item.spec.containers[0].command, v, namespace)
+                './conf/kube-apiserver', headers, v, l)                
+                json_data = k8s.CtrlProp.check_admission_controllers(\
+                item.spec.containers[0].command, v, namespace, l)
+
+                if l: _logger.info(json_data)
 
             elif item.spec.containers[0].name in "kube-scheduler" \
             and item.spec.containers[0].name not in container_name_check:
                 CtrlPlane.check_ctrl_plane_pods_properties_operation(item,\
-                './conf/kube-scheduler', headers, v)  
+                './conf/kube-scheduler', headers, v, l)  
                 k8s.CtrlProp.secure_scheduler_check(\
                 item.spec.containers[0].command)
             container_name_check = item.spec.containers[0].name
 
-def call_all(v):
-    CtrlPlane.get_ctrl_plane_pods()
-    CtrlPlane.check_ctrl_plane_security(v)
-    CtrlPlane.check_ctrl_plane_pods_health_probes(v)
-    CtrlPlane.check_ctrl_plane_pods_resources(v)
-    CtrlPlane.check_ctrl_plane_pods_qos(v)
-    CtrlPlane.check_ctrl_plane_pods_properties(v)
+def call_all(v, l):
+    CtrlPlane.get_ctrl_plane_pods(l)
+    CtrlPlane.check_ctrl_plane_security(v, l)
+    CtrlPlane.check_ctrl_plane_pods_health_probes(v, l)
+    CtrlPlane.check_ctrl_plane_pods_resources(v, l)
+    CtrlPlane.check_ctrl_plane_pods_qos(v, l)
+    CtrlPlane.check_ctrl_plane_pods_properties(v, l)
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hv", ["help", "verbose"])
+        opts, args = getopt.getopt(sys.argv[1:], \
+        "hvl", ["help", "verbose", "logging"])
         if not opts:        
-            call_all(False)
+            call_all(False, '')
             k8s.Output.time_taken(start_time)
             sys.exit()
             
@@ -108,15 +113,17 @@ def main():
         print(err)
         return
 
+    verbose, l = '', ''
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
         elif o in ("-v", "--verbose"):
             verbose = True
-            call_all(verbose)
+        elif o in ("-l", "--logging"):
+            l = True             
         else:
             assert False, "unhandled option"
-
+    call_all(verbose, l)
     k8s.Output.time_taken(start_time)     
 
 if __name__ == "__main__":
