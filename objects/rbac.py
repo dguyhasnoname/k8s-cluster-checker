d2ed4f60d16d58a573cd6b7426061fbed97aac5e
dguyhasnoname <dguyhasnoname@gmail.com>
2020-08-22 23:07:19 +0530

    refactored for logging

--- objects/rbac.py
+++ objects/rbac.py
@@ -1,6 +1,7 @@
 import sys, time, os, getopt, argparse, re
 from concurrent.futures import ThreadPoolExecutor
 start_time = time.time()
+from modules import logging as logger
 from modules import process as k8s
 from modules.get_rbac import K8sClusterRole, K8sClusterRoleBinding, \
 K8sNameSpaceRole, K8sNameSpaceRoleBinding
@@ -15,7 +16,8 @@ class ClusterRBAC:
 
         # pulling rbac data in threads for fast execution
         global cluster_role_list, cluster_role_binding_list, ns_role_list, \
-        ns_role_binding_list
+        ns_role_binding_list, _logger
+        _logger = logger.get_logger('Namespace')
         with ThreadPoolExecutor(max_workers=5) as executor:      
             tmp_cluster_role_list = \
             executor.submit(K8sClusterRole.list_cluster_role)
@@ -31,14 +33,14 @@ class ClusterRBAC:
         ns_role_list = tmp_ns_role_list.result()
         ns_role_binding_list = tmp_ns_role_binding_list.result()
 
-    def get_rbac_count():
+    def get_rbac_count(v, l):
         headers = ['CLUSTER_ROLE', 'CLUSTER_ROLE_BINDING', 'ROLE', \
         'ROLE_BINDING']
         k8s.Output.print_table([[len(cluster_role_list.items), \
         len(cluster_role_binding_list.items), len(ns_role_list.items), \
-        len(ns_role_binding_list.items)]], headers, True) 
+        len(ns_role_binding_list.items)]], headers, True, l) 
        
-    def get_cluster_role(v):
+    def get_cluster_role(v, l):
         k8s_object = "clusteroles"
         data = []
         headers = ['CLUSTER_ROLE', 'RULES', 'API_GROUPS', 'RESOURCES', 'VERBS']
@@ -50,13 +52,13 @@ class ClusterRBAC:
                 rules[0], rules[1], rules[2]])
             else:
                 data.append([item.metadata.name, "-", "-", "-", "-"])
-        k8s.Rbac.analyse_role(data, headers, k8s_object, 'all') 
+        k8s.Rbac.analyse_role(data, headers, k8s_object, 'all', l) 
         data = k8s.Output.append_hyphen(data, '-----------')
         data.append(["Total: " + str(len(cluster_role_list.items)), \
         rules[3], "-", "-", "-"])
-        k8s.Output.print_table(data,headers,v)
+        k8s.Output.print_table(data, headers, v, l)
 
-    def get_cluster_role_binding(v): 
+    def get_cluster_role_binding(v, l): 
         data, rules_count = [], 0
         headers = ['CLUSTER_ROLE_BINDING', 'CLUSTER_ROLE', \
         'SERVICE_ACCOUNT', 'NAMESPACE']
@@ -71,12 +73,13 @@ class ClusterRBAC:
         data = k8s.Output.append_hyphen(data, '-----------')
         data.append(["Total: " + str(len(cluster_role_binding_list.items)), \
         "-", "-", "-"])
-        k8s.Output.print_table(data,headers,v)
+        k8s.Output.print_table(data, headers, v, l)
         k8s.Output.csv_out(data, headers, 'rbac', 'cluster_role_binding', 'all')
-        json_data = k8s.Output.json_out(data, headers, 'rbac', \
-        'cluster_role_binding', 'all')          
+        json_data = k8s.Output.json_out(data[:-2], '', headers, 'rbac', \
+        'cluster_role_binding', 'all')
+        _logger.info(json_data)          
 
-    def get_ns_role(v):    
+    def get_ns_role(v, l):    
         data = []
         k8s_object = 'roles'
         headers = ['ROLE', 'NAMESPACE', 'RULES', 'API_GROUPS', 'RESOURCES', 'VERBS']
@@ -88,13 +91,13 @@ class ClusterRBAC:
             else:
                 data.append([item.metadata.name, item.metadata.namespace, \
                 "-", "-", "-", "-"])
-        k8s.Rbac.analyse_role(data, headers, k8s_object, namespace)
+        k8s.Rbac.analyse_role(data, headers, k8s_object, namespace, l)
         data = k8s.Output.append_hyphen(data, '---------')
         data.append(["Total: " + str(len(ns_role_list.items)), \
         "-", "-", "-",  "-", "-"])          
-        k8s.Output.print_table(data,headers,v)
+        k8s.Output.print_table(data, headers, v, l)
 
-    def get_ns_role_binding(v):      
+    def get_ns_role_binding(v, l):      
         data = []
         headers = ['ROLE_BINDING', 'NAMESPACE', 'ROLE', 'GROUP_BINDING']
         for item in ns_role_binding_list.items:
@@ -113,33 +116,34 @@ class ClusterRBAC:
         data = k8s.Output.append_hyphen(data, '---------')
         data.append(["Total: " + str(len(ns_role_binding_list.items)), \
         "-", "-", "-"]) 
-        k8s.Output.print_table(data,headers,v)
+        k8s.Output.print_table(data, headers, v, l)
         k8s.Output.csv_out(data, headers, 'rbac', 'ns_role_binding', namespace)
-        json_data = k8s.Output.json_out(data, headers, 'rbac', \
-        'ns_role_binding', namespace)    
+        json_data = k8s.Output.json_out(data[:-2], '', headers, 'rbac', \
+        'ns_role_binding', namespace)
+        _logger.info(json_data)  
 
-def call_all(v,ns):
+def call_all(v, ns, l):
     ClusterRBAC(ns)
-    ClusterRBAC.get_rbac_count()      
+    ClusterRBAC.get_rbac_count(v, l)      
     if not ns:
-        ClusterRBAC.get_cluster_role(v)
-        ClusterRBAC.get_cluster_role_binding(v)
-    ClusterRBAC.get_ns_role(v)
-    ClusterRBAC.get_ns_role_binding(v)
+        ClusterRBAC.get_cluster_role(v, l)
+        ClusterRBAC.get_cluster_role_binding(v, l)
+    ClusterRBAC.get_ns_role(v, l)
+    ClusterRBAC.get_ns_role_binding(v, l)
 
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], \
-        "hvn:", ["help", "verbose", "namespace"])
+        "hvn:l", ["help", "verbose", "namespace", "logging"])
         if not opts:        
-            call_all("","")
+            call_all('', '', '')
             k8s.Output.time_taken(start_time)
             sys.exit()
             
     except getopt.GetoptError as err:
         print(err)
         return
-    verbose, ns = '', ''
+    verbose, ns, l = '', '', ''
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
@@ -147,10 +151,12 @@ def main():
             verbose = True
         elif o in ("-n", "--namespace"):
             if not verbose: verbose = False
-            ns = a          
+            ns = a
+        elif o in ("-l", "--logging"):
+            l = True                   
         else:
             assert False, "unhandled option"
-    call_all(verbose,ns)
+    call_all(verbose, ns, l)
     k8s.Output.time_taken(start_time)     
 
 if __name__ == "__main__":
