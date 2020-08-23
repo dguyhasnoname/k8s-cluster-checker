8ba24d4825899c840213666ad0c0f92144d797b2
dguyhasnoname <dguyhasnoname@gmail.com>
2020-08-23 09:05:05 +0530

    updated logging

--- objects/jobs.py
+++ objects/jobs.py
@@ -1,6 +1,7 @@
 import sys, time, os, getopt
-from modules import process as k8s
 start_time = time.time()
+from modules import logging as logger
+from modules import process as k8s
 from modules.get_jobs import K8sJobs
 
 class Jobs:
@@ -14,73 +15,80 @@ class Jobs:
         if not len(k8s_object_list.items):
             print ("[WARNING] No jobs found.")
             sys.exit()
-    global k8s_object
+    global k8s_object, _logger
+    _logger = logger.get_logger('Jobs')
     k8s_object = 'jobs'
 
-    def list_jobs(v):
+    def list_jobs(v, l):
         data = []
         headers = ['NAMESPACE', 'JOBS']
         for item in k8s_object_list.items:
             data.append([item.metadata.namespace, item.metadata.name])
         data = k8s.Output.append_hyphen(data, '---------')
         data.append(["Total: " , len(data) - 1])
-        k8s.Output.print_table(data, headers, True)
+        k8s.Output.print_table(data, headers, True, l)
 
-    def check_jobs_pod_security(v):
+    def check_jobs_pod_security(v, l):
         headers = ['NAMESPACE', 'JOBS', 'CONTAINER_NAME', 'PRIVILEGED_ESC', \
         'PRIVILEGED', 'READ_ONLY_FS', 'RUN_AS_NON_ROOT', 'RUNA_AS_USER']        
-        k8s.Check.security_context(k8s_object, k8s_object_list, headers, \
-        v, namespace)
+        data = k8s.Check.security_context(k8s_object, k8s_object_list, headers, \
+        v, namespace, l)
+        if l: _logger.info(data)
 
-    def check_jobs_pod_health_probes(v):
+    def check_jobs_pod_health_probes(v, l):
         headers = ['NAMESPACE', 'JOBS', 'CONTAINER_NAME', 'READINESS_PROPBE', \
         'LIVENESS_PROBE']        
-        k8s.Check.health_probes(k8s_object, k8s_object_list, headers, \
-        v, namespace)  
+        data = k8s.Check.health_probes(k8s_object, k8s_object_list, headers, \
+        v, namespace, l)
+        if l: _logger.info(data) 
 
-    def check_jobs_pod_resources(v): 
+    def check_jobs_pod_resources(v, l): 
         headers = ['NAMESPACE', 'JOBS', 'CONTAINER_NAME', 'LIMITS', 'REQUESTS']       
-        k8s.Check.resources(k8s_object, k8s_object_list, headers, v, namespace)         
+        data = k8s.Check.resources(k8s_object, k8s_object_list, headers, \
+        v, namespace, l)
+        if l: _logger.info(data)         
 
-    def check_jobs_pod_tolerations_affinity_node_selector_priority(v): 
+    def check_jobs_pod_tolerations_affinity_node_selector_priority(v, l): 
         headers = ['NAMESPACE', 'JOBS', 'NODE_SELECTOR', 'TOLERATIONS', \
         'AFFINITY', 'PRIORITY_CLASS']     
-        k8s.Check.tolerations_affinity_node_selector_priority(k8s_object, \
-        k8s_object_list, headers, v, namespace)
+        data = k8s.Check.tolerations_affinity_node_selector_priority(k8s_object, \
+        k8s_object_list, headers, v, namespace, l)
+        if l: _logger.info(data)
 
-def call_all(v,ns):
+def call_all(v, ns, l):
     Jobs(ns)
-    Jobs.check_jobs_pod_security(v)
-    Jobs.check_jobs_pod_health_probes(v)
-    Jobs.check_jobs_pod_resources(v)
-    Jobs.check_jobs_pod_tolerations_affinity_node_selector_priority(v)
-    Jobs.list_jobs(v)
+    Jobs.list_jobs(v, l)
+    Jobs.check_jobs_pod_security(v, l)
+    Jobs.check_jobs_pod_health_probes(v, l)
+    Jobs.check_jobs_pod_resources(v, l)
+    Jobs.check_jobs_pod_tolerations_affinity_node_selector_priority(v, l)
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hvn:", \
-        ["help", "verbose", "namespace"])
+        opts, args = getopt.getopt(sys.argv[1:], "hvn:l", ["help", "verbose", \
+        "namespace", "logging"])
         if not opts:        
-            call_all("","")
+            call_all('','','')
             k8s.Output.time_taken(start_time)
             sys.exit()
             
     except getopt.GetoptError as err:
         print(err)
         return
-    verbose, ns = '', ''
+    verbose, ns, l= '', '', ''
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
         elif o in ("-v", "--verbose"):
             verbose = True
         elif o in ("-n", "--namespace"):
-            if not verbose: verbose = False
-            ns = a          
+            ns = a
+        elif o in ("-l", "--logging"):
+            l = True                  
         else:
             assert False, "unhandled option"
-    call_all(verbose,ns)
-    k8s.Output.time_taken(start_time)     
+    call_all(verbose, ns, l)
+    k8s.Output.time_taken(start_time)      
 
 if __name__ == "__main__":
     try:
