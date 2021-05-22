33236b2799378bd9df034a7888dbeab4414e512e
dguyhasnoname <dguyhasnoname@gmail.com>
2021-05-22 17:11:13 +0530

    updated logging

--- objects/modules/get_cm.py
+++ objects/modules/get_cm.py
@@ -6,15 +6,15 @@ kubeConfig.load_kube_config()
 core = client.CoreV1Api()
 
 class K8sConfigMap:
-    def get_cm(ns):
+    def get_cm(ns, logger):
         try:
             if ns != 'all':
                 #print ("\n[INFO] Fetching {} namespace configMaps data...".format(ns))
                 namespace = ns
                 configmaps = core.list_namespaced_config_map(namespace, timeout_seconds=10)
             else:
-                print ("\n[INFO] Fetching all namespace configMaps data...")
+                logger("Fetching all namespace configMaps data.")
                 configmaps = core.list_config_map_for_all_namespaces(timeout_seconds=10)
             return configmaps
         except ApiException as e:
-            print("Exception when calling CoreV1Api->list_namespaced_config_map: %s\n" % e)
+            logger("Exception when calling CoreV1Api->list_namespaced_config_map: %s\n" % e)
