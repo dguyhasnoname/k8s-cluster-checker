f372f0f32f47071be73dc73d7b626edbcc3f431d
dguyhasnoname <dguyhasnoname@gmail.com>
2021-05-23 10:50:41 +0530

    added new field logger

--- objects/modules/main.py
+++ objects/modules/main.py
@@ -2,10 +2,10 @@ import getopt, sys
 
 class GetOpts:
     def get_opts():
-        u, verbose, ns, l= '', '', '', ''
+        u, verbose, ns, l, format = [''] * 5
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "hvn:l", ["help", "verbose", \
-            "namespace", "logging"])
+            opts, args = getopt.getopt(sys.argv[1:], "hvn:lf:", ["help", "verbose", \
+            "namespace", "logging", 'format'])
         except getopt.GetoptError as err:
             print("[ERROR] {}. ".format(err) + \
             "Please run script with -h flag to see valid options.")
@@ -19,9 +19,11 @@ class GetOpts:
             elif o in ("-n", "--namespace"):
                 ns = a
             elif o in ("-l", "--logging"):
-                l = True                  
+                l = True
+            elif o in ("-f", "--format"):
+                l = True                                   
             else:
                 assert False, "unhandled option" 
 
-        options = [u, verbose, ns, l]
+        options = [u, verbose, ns, l, format]
         return options 
