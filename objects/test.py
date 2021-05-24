fda420e03410941369be888b8a2f7a0514f65f96
dguyhasnoname <dguyhasnoname@gmail.com>
2021-05-25 00:17:30 +0530

    suppressed warning for SSL

--- objects/test.py
+++ objects/test.py
@@ -7,20 +7,24 @@ logger = Logger.get_logger('', '')
 class TestK8sClusterChecker(unittest.TestCase):
     def test_cluster(self):
         import cluster as cluster
+        warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>")
         self.assertRaises(TypeError, cluster.call_all(True, True, logger), True)
         with self.assertRaises(Exception) as x:
             print("Exception ignored: {}".format(x.exception))
     def test_nodes(self):
         import nodes as nodes
+        warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>")
         with self.assertRaises(Exception) as x:
             print("Exception ignored: {}".format(x.exception))
     def test_namespace(self):
         import namespace as namespace
+        warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>")
         self.assertRaises(TypeError, namespace.call_all(True, 'all', True, logger), True)
         with self.assertRaises(Exception) as x:
             print("Exception ignored: {}".format(x.exception))
     def test_pods(self):
         import pods as pods
+        warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>")
         self.assertRaises(TypeError, pods.call_all(True, 'all', True, logger), True)
         with self.assertRaises(Exception) as x:
             print("Exception ignored: {}".format(x.exception))
