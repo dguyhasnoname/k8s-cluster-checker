f5300a997f828f5f0333d4da7d8876be7e2fb61f
dguyhasnoname <dguyhasnoname@gmail.com>
2021-06-19 23:58:19 +0530

    fixed sa logic

--- objects/serviceaccounts.py
+++ objects/serviceaccounts.py
@@ -1,42 +1,39 @@
 import time, os, json, sys
 start_time = time.time()
-from modules.main import GetOpts
-from modules import logging as logger
+from modules.main import ArgParse
+from modules.logging import Logger
 from modules import process as k8s
 from modules.get_svc_acc import K8sSvcAcc
 
 class ServiceAccount:
-    def __init__(self,ns):
-        global k8s_object_list, namespace
-        self.ns = ns
-        if not ns:
-            ns = 'all'
-        namespace = ns   
-        k8s_object_list = K8sSvcAcc.get_svc_acc(ns) 
-    global k8s_object, _logger
-    _logger = logger.get_logger('ServiceAccount')
-    k8s_object = 'serviceaccount'
+    def __init__(self, namespace, logger):
+        self.namespace = namespace
+        self.logger = logger
+        if not self.namespace:
+            self.namespace = 'all'
+        self.k8s_object_list = K8sSvcAcc.get_svc_acc(self.namespace) 
+        self.k8s_object = 'serviceaccount'
 
-    def get_namespaced_pod_list(v, l):
+    def get_namespaced_sa_list(self, v, l):
         data = []
         headers = ['NAMESPACE', 'SERVICE_ACCOUNT', 'SECRET']
-        for item in k8s_object_list.items:
+        for item in self.k8s_object_list.items:
             for j in item.secrets:
                 data.append([item.metadata.namespace, item.metadata.name, j.name])
         if v: print ("Total service accounts: {}".format(len(data)))            
         k8s.Output.print_table(data, headers, True, l)
         return data
 
-def call_all(v, ns, l):
-    ServiceAccount(ns)
-    ServiceAccount.get_namespaced_pod_list(v, l)
+def call_all(v, namespace, l, logger):
+    call = ServiceAccount(namespace, logger)
+    call.get_namespaced_sa_list(v, l)
 
 def main():
-    options = GetOpts.get_opts()
-    if options[0]:
-        usage()
-    if options:
-        call_all(options[1], options[2], options[3])
+    args = ArgParse.arg_parse()
+    # args is [u, verbose, ns, l, format, silent]
+    logger = Logger.get_logger(args.format, args.silent)
+    if args:
+        call_all(args.verbose, args.namespace, args.logging, logger)
         k8s.Output.time_taken(start_time)   
 
 if __name__ == "__main__":
